111fca3ef8e9187ff14656dc156d3e5c24972818
Kevin-Ni0 <158388153+Kevin-Ni0@users.noreply.github.com>
2025-03-18 02:30:13 +0000

    point, rectangle cpp and makefile fix

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,19 @@
 #include "Point.h"
 
 using namespace std;
+
+Point::Point(double xCoord, double yCoord) {
+    x=xCoord;
+    y=yCoord;
+}
+
+double Point::distanceTo(const Point& other) const {
+    double dx = other.x - x;
+    double dy = other.y - y;
+    return sqrt(pow(dx, 2) + pow(dy, 2));
+}
+ 
+
+void Point::print() const{
+cout <<x <<" , "<< y;
+}
